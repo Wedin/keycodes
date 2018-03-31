a92a8500929af21eccce4eca24f8ed9ba515fe43
Daniel Wedin <danielwedin@hotmail.se>
2018-04-01 00:10:18 +0200

    Add missing meta tags

--- pages/_document.js
+++ pages/_document.js
@@ -47,6 +47,8 @@ export default class MyDocument extends Document {
       <html lang="en">
         <Head>
           <title>Keycodes</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta charSet="utf-8" />
           {this.props.styleTags}
         </Head>
         <body>
